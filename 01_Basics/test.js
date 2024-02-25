8067f96830c29a30e050e446ec91cdd62a2beacf
Kuldeep Singh <victorkuldeep@gmail.com>
2024-02-25 21:41:44 +0530

    01 Basics Adding

---
+++ 01_Basics/test.js
@@ -0,0 +1,10 @@
+console.log("Exploring Java Script")
+// Defining an Object
+let student  = {
+    firstName : "Kuldeep",
+    lastName : "Singh",
+    city : "Noida"
+}
+// console.log(student.city); // One way of accessing
+// console.log(student["city"]); // Another way of accessing
+// console.log(typeof student);
